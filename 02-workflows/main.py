ce62d5e9d5992c7f32a7ddef429f5a53f7ddc9a6
Aleksandar Ristic <storiaca@gmail.com>
2025-07-06 15:58:15 +0200

    Getting started with a firts example workflow

--- 02-workflows/main.py
+++ 02-workflows/main.py
@@ -1,6 +1,11 @@
+def generate_x_post(usr_input: str) -> str:
+    # call AI / LLm
+    pass
 def main():
-    print("Hello from 02-workflows!")
-
-
+    # user input => AI (LLM) to generate X post => output post
+    usr_input = input("What should post be about?")
+    x_post = generate_x_post(usr_input)
+    print("Generate X post")
+    print((x_post))
 if __name__ == "__main__":
     main()
