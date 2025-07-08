d8657ef7b85b65d6724234afd5cf49ca66734339
Aleksandar Ristic <storiaca@gmail.com>
2025-07-08 19:14:31 +0200

    Preparing HTTP Request for the OpenAI api

--- 02-workflows/main.py
+++ 02-workflows/main.py
@@ -1,5 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 def generate_x_post(usr_input: str) -> str:
-    # call AI / LLm
+    # call AI / LLM
+    payload = {
+        "model": "...S",
+        "input": "..."
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/responses", 
+        json=payload,
+        headers= {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {OPENAI_API_KEY}"
+        }
+    )
     pass
 def main():
     # user input => AI (LLM) to generate X post => output post
