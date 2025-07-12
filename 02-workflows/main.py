a1f59d486c8e6b1808e48fef6b1f04b92a3ebf9a
Aleksandar Ristic <storiaca@gmail.com>
2025-07-12 12:24:52 +0200

    Leveraging Few-Shor Prompting

--- 02-workflows/main.py
+++ 02-workflows/main.py
@@ -19,6 +19,39 @@ def generate_x_post(topic: str) -> str:
     <topic>
     {topic}
     </topic>
+
+    Here are some examples of topic and generated posts:
+    <examples-1>
+        <topic>
+        Open LLMs are great because they are more than enough for many workflows and offer free use & 100% privacy!
+        </topic>
+
+        <generated-post>
+        Yes, Gemini 2.5 Pro is amazing. But for many tasks, it's too expensive & simply overkill.
+
+        Don't sleep on open LLMs which you can run locally on your MacBook!
+
+        Open LLMs like Gemma 3 can be run locally via Ollama or LM Studio, offer 100% privacy and are more than capable enough for most use-cases and workflows.
+        </generated-post>
+    </examples-1>
+
+     <examples-2>
+        <topic>
+        Despite LLms: Learn to code! Because AI-assisted coding > Vibe coding.
+        </topic>
+
+        <generated-post>
+        There's never been a better time to learn coding! Seriously!
+
+        Yes, you can vibe code sloppy apps all day.
+
+        If you want to build something that acctually works, you need to learn to code though.
+        Combine that with AI assistants like Copilot and nothing's going to stop you!
+        </generated-post>
+    </examples-2>
+
+    Please use the tone, language, structure, and style of the examples provided above to generate a post that is engaging and relevant to the topic provided by the user.
+    Don't use the content from the examples!
 """
     response = client.responses.create(
         model="gpt-4o",
