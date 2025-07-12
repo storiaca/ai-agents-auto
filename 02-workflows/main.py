b6a514379a7ed838551d9f1b3cba46863de3e73e
Aleksandar Ristic <storiaca@gmail.com>
2025-07-12 12:48:37 +0200

    Generating Prompts Dynamically with dynamic content

--- 02-workflows/main.py
+++ 02-workflows/main.py
@@ -1,12 +1,32 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
 client = OpenAI()
 
 def generate_x_post(topic: str) -> str:
-    # call AI / LLM
+    with open("post-examples.json", "r") as f:
+        examples = json.load(f)
+    # Prepare examples for the prompt
+    # examples_str = "\n\n".join(
+    #     f"<topic>\n{example['topic']}\n</topic>\n\n<generated-post>\n{example['post']}\n</generated-post>"
+    #     for example in examples
+    # )
+    examples_str = ""
+    for i, example in enumerate(examples, 1):
+        examples_str += f"""
+        <example-{i}>
+           <topic>
+           {example['topic']}
+           </topic>
+
+           <generated-post>
+           {example['post']}
+           </generated-post>
+        </example-{i}>
+        """
     prompt = f"""
     You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).
 
@@ -21,34 +41,9 @@ def generate_x_post(topic: str) -> str:
     </topic>
 
     Here are some examples of topic and generated posts:
-    <examples-1>
-        <topic>
-        Open LLMs are great because they are more than enough for many workflows and offer free use & 100% privacy!
-        </topic>
-
-        <generated-post>
-        Yes, Gemini 2.5 Pro is amazing. But for many tasks, it's too expensive & simply overkill.
-
-        Don't sleep on open LLMs which you can run locally on your MacBook!
-
-        Open LLMs like Gemma 3 can be run locally via Ollama or LM Studio, offer 100% privacy and are more than capable enough for most use-cases and workflows.
-        </generated-post>
-    </examples-1>
-
-     <examples-2>
-        <topic>
-        Despite LLms: Learn to code! Because AI-assisted coding > Vibe coding.
-        </topic>
-
-        <generated-post>
-        There's never been a better time to learn coding! Seriously!
-
-        Yes, you can vibe code sloppy apps all day.
-
-        If you want to build something that acctually works, you need to learn to code though.
-        Combine that with AI assistants like Copilot and nothing's going to stop you!
-        </generated-post>
-    </examples-2>
+    <examples>
+        {examples_str}
+    </examples>
 
     Please use the tone, language, structure, and style of the examples provided above to generate a post that is engaging and relevant to the topic provided by the user.
     Don't use the content from the examples!
